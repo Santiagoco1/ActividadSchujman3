1b0ff4cbed3302727c2b0f56b19f563f04ed57d7
Santiago Có <51292523+Santiagoco1@users.noreply.github.com>
2020-05-23 15:27:58 -0300

    Add files via upload

--- Actividad3/Actividad 3/javascript/guardar.php
+++ Actividad3/Actividad 3/javascript/guardar.php
@@ -6,7 +6,7 @@ $mysqli = new mysqli($HOST, $USER, $PASS, $DB,$TABLA);
 
 
 
-	/* Crea la tabla si esta no existe. La tabulacion es para que quede mas lindo. En total 24*/ 
+	/* Crea la tabla si esta no existe. La tabulacion es para que quede mas lindo. En total 24 
 	$registro = $mysqli_query("create table if not exists $TABLA (
 		apellido		varchar(60),
 		nombre			varchar(60),
@@ -30,19 +30,21 @@ $mysqli = new mysqli($HOST, $USER, $PASS, $DB,$TABLA);
 		talle_zapato	int,
 		cuando	 		timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP(),
 		orden			int not null primary key auto_increment
-	)");
+	)");*/
 	/* Mete las cosas en la tabla */
-	$mysqli->query("insert into $TABLA (apellido, nombre, fecha_nacimien, dni, domicilio, nro_emer, nro_obrasocial,
+	$mysqli->query("insert into $TABLA (apellido, nombre, fecha_nacimien, dni, domicilio,  
 							grupo_sanguineo, factor, peso, altura, email, telefono_fijo, telefono_celu, 
-							telefono_altern, enfermedades, medicamentos, foto_cara, color_fav, talle_zapato)
-					values ('".$_POST['apellido']."','".$_POST['nombre'].','"".$_POST['fecha_nacimien'].','"
-							".$_POST['dni']."','".$_POST['domicilio']."','".$_POST['nro_emer']."','
-							".$_POST['nro_obrasocial']."','".$_POST['grupo_sanguineo']."','".$_POST['factor']."','
-							".$_POST['peso']."','".$_POST['altura']."','
-							".$_POST['email']."','".$_POST['telefono_fijo']."','".$_POST['telefono_celu']."','
-							".$_POST['telefono_altern']."','".$_POST['enfermedades']."','".$_POST['medicamentos']."','
-							".$_POST['foto_cara']."','".$_POST['color_fav']."','".$_POST['talle_zapato']."')");
+							telefono_altern, enfermedades,   color_fav, talle_zapato)
+					values ('".$_POST['apellido']."','".$_POST['nombre'].','"".$_POST['fecha_nacimien'].',
+							'"".$_POST['dni']."','".$_POST['domicilio']."',
+							'".$_POST['grupo_sanguineo']."','".$_POST['factor']."',
+							'".$_POST['peso']."','".$_POST['altura']."','
+							'".$_POST['email']."','".$_POST['telefono_fijo']."','".$_POST['telefono_celu']."',
+							'".$_POST['telefono_altern']."','".$_POST['enfermedades']."',
+							'".$_POST['color_fav']."','".$_POST['talle_zapato']."')");
+						
+							/*foto_cara, nro_emer, nro_obrasocial, medicamentos,
+							'".$_POST['nro_emer']."','".$_POST['nro_obrasocial']."',
+							'".$_POST['foto_cara']."', '".$_POST['medicamentos']."', */
 
-
-mysqli_close($conn);
 ?>
