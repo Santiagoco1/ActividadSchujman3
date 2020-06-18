7cbe74491a115c292686e86f430c250d307a998f
Franco <fgozzerino@gmail.com>
2020-06-18 17:15:50 -0300

    Primer intento de mandar un mail al registrarse (todavia no se probo la version anterior)

--- Actividad3/registro.php
+++ Actividad3/registro.php
@@ -6,5 +6,12 @@
     
     $mysqli->query("insert into Test1 ( contra, nombre, email) values ( '".$_POST['contra']."' , '".$_POST['nombre']."' , '".$_POST['email']."')" );
 
+    $para = '".$_POST['email']."'
+    $titulo    = 'Validacion de registro';
+    $mensaje   = 'Hola gato';
+    $cabeceras = 'From: grupoUNO@example.com' . "\r\n" .
+    'Reply-To: grupoUNO@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
 
+    mail($para, $titulo, $mensaje, $cabeceras);
 ?>
