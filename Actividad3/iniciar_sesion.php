a18dedfc30bfcca5ee487dfc17a3fc96b1fd78df
Franco <fgozzerino@gmail.com>
2020-06-18 17:41:28 -0300

    Primero intento de iniciar sesion y guardar datos en la cookie (No se si funciona)

---
+++ Actividad3/iniciar_sesion.php
@@ -0,0 +1,11 @@
+<?php
+
+    include "var.inc";
+
+    $mysqli = new mysqli($HOST, $USER, $PASS, $DB);
+    
+    $value = $mysqli->query("SELECT orden from Test1 where contra = '".$_POST['contra']."' and nombre =  '".$_POST['nombre']."' and email = '".$_POST['email']."') " );
+
+    setcookie("User", $value);
+    
+?>
