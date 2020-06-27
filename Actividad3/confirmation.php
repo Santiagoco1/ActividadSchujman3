77e0418583b5d69e71314bfcb1ea1e2ed7303626
Franco <fgozzerino@gmail.com>
2020-06-27 14:27:42 -0300

    Version que funca la confirmacion por mail

--- Actividad3/confirmation.php
+++ Actividad3/confirmation.php
@@ -1,5 +1,37 @@
 <?php
     include "var.inc";
     $mysqli = new mysqli($HOST, $USER, $PASS, $DB);
+    if($_GET)
+    {
+        if(isset($_GET['email']))
+        {
+            $email = $_GET['email'];
+            if($email == '')
+            {
+                unset($email);
+            }
+        }
+        if(isset($_GET['token']))
+        {
+            $token = $_GET['token'];
+            if($token == '')
+            {
+                unset($token);
+            }
+        }
+        if(!empty($email) && !empty($token))
+        {   
+            echo '<script>alert("' . $email . ' ")</script> ';
+            
+            if($mysqli->query("UPDATE $TABLA SET confirmacion = 1  WHERE email = '$email' ") === true){ 
+                echo '<script>alert("Funco")</script> ';
+                     
+            } else { 
+                echo '<script>alert("No funco")</script> ';
+                     
+            }  
+            echo "<script>location.href='sing_in.php'</script>";
+        }
+    }
     
 ?> 
--- Actividad3/validate_user.php
+++ Actividad3/validate_user.php
@@ -7,51 +7,42 @@
             $pass = $_POST['contra'];
    
             $sql = mysqli_query($mysqli,"SELECT * FROM $TABLA WHERE email ='$username'");
-            if($f = mysqli_fetch_assoc($sql))
+            
+            if( $f = mysqli_fetch_assoc($sql) )
+            {
+
+                $user = $f['nombre'];
+                $orden = $f['orden'];
+                
+                /* Seteo las cookies */
+                setcookie("user", $user );
+                setcookie("id", $orden );
+                
+                $cuantos_logueos = $f['cuantos_logueos'] + 1 ;
+                echo '<script>alert("' . $cuantos_logueos .'")</script> ';
+                $mysqli->query("UPDATE $TABLA SET cuantos_logueos = '$cuantos_logueos'  WHERE email = '$orden' ");
+
+                if($pass == $f['passadmin'])
                 {
-                    if($pass == $f['passadmin'])
-                        {
-                            /* Seteo las cookies 
-                            
-                             */
-                            
-                            setcookie("id", $f['orden']);
-                            setcookie("user", $f['nombre']);
-                            ++ $f['cuantos_logueos'] ;
-                            $f['ultimo_logueo'] = new DateTime();
-                            $f['ultimo_logueo'] = getTimestamp();
-                            $mysqli->query("insert into $TABLA (cuantos_logueos , ultimo_logueo) values ('++ $f['cuantos_logueos']' , '$f['ultimo_logueo']' )" );
-                            
-                            echo '<script>alert("BIENVENIDO ADMINISTRADOR")</script> ';
-                            echo "<script>location.href='admin-responses.php'</script>";
-                        
-                        }
-                    else
-                        if($pass == $f['contra'])
-                            {
-
-                                /* Seteo las cookies  */
-                                setcookie("user", $f['nombre']);
-                                setcookie("id", $f['orden']);
-                                
-                                ++ $f['cuantos_logueos'] ;
-                                $f['ultimo_logueo'] = new DateTime();
-                                $f['ultimo_logueo'] = getTimestamp();
-                                $mysqli->query("insert into $TABLA (cuantos_logueos , ultimo_logueo) values ( '++ $f['cuantos_logueos']' , '$f['ultimo_logueo']' )" );
-
-                                echo "<script>location.href='user-form.php'</script>";
-                        
-                        /*      header("Location: user-formulario.html");*/
-                            }
-                        else
-                            {
-                                echo '<script>alert("CONTRASEÑA INCORRECTA")</script> ';
-                                echo "<script>location.href='sign-in.php'</script>";
-                            }
+                    echo '<script>alert("BIENVENIDO ADMINISTRADOR")</script> ';
+                    echo "<script>location.href='admin_responses.php'</script>";
                 }
+                else 
+                {
+                    if($pass == $f['contra'])
+                    {
+                        echo "<script>location.href='user_form.php'</script>";
+                    }
                     else
-                        {
-                            echo '<script>alert("ESTE USUARIO NO EXISTE, PORFAVOR REGISTRESE PARA PODER INGRESAR")</script> ';   
-                            echo "<script>location.href='sign-in.php'</script>";	
-                        }
+                    {
+                        echo '<script>alert("CONTRASEÑA INCORRECTA")</script> ';
+                        echo "<script>location.href='sing_in.php'</script>";
+                    }
+                }
+            }    
+            else
+            {
+                echo '<script>alert("ESTE USUARIO NO EXISTE, PORFAVOR REGISTRESE PARA PODER INGRESAR")</script> ';   
+                echo "<script>location.href='sing_in.php'</script>";	
+            }
 ?>
